04560c0395ccdc1acfd8472afa450491f9890a34
vansang1012 <giapvansang1012@gmail.com>
2019-08-01 10:25:30 +0700

    layout1

---
+++ home.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="mystyle.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div class="container">
+    <header><?php  include "header.php";  ?></header>
+    <nav><?php include "nav.php";  ?></nav>
+    <article> <?php include "content.php"; ?></article>
+    <footer><?php include "footer.php";?></footer>
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,5 @@
+<ul>
+    <li><a href="#">Lon Don</a> </li>
+    <li><a href="#">Pari</a> </li>
+    <li><a href="#">Tokyo</a> </li>
+</ul>
